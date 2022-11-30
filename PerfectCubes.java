983625703f08eaabc7a09b1a5760dcb56ef9f78c
andriibessarab <andriibessarab@gmail.com>
2022-11-30 13:37:58 -0500

    3.6 - PerfectCubes

---
+++ PerfectCubes.java
@@ -0,0 +1,8 @@
+public class PerfectCubes {
+    public static void main(String[] args) {
+        // print all perfect cubes from 1 to 125000
+        for (int i = 1; i <= 50; i++) {
+            System.out.println((int)Math.pow(i, 3));
+        }
+    }
+}
