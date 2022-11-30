abc896d3ccaa4962f55a26f47ff3dbf70e45ee74
andriibessarab <andriibessarab@gmail.com>
2022-11-30 09:49:17 -0500

    3.2 - Initials2

---
+++ Initials2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Initials2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scan.nextLine();
+        scan.close();
+        System.out.println(name.charAt(0) + "." + name.charAt(name.indexOf(" ") + 1) + "." + name.charAt(name.lastIndexOf(" ") + 1) + ".");
+    }
+}
