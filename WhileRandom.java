5663e9928073f866797bcfde9575453616760a9d
andriibessarab <andriibessarab@gmail.com>
2022-11-30 12:15:20 -0500

    3.4 - WhileRandom - v2

--- WhileRandom.java
+++ WhileRandom.java
@@ -2,7 +2,12 @@ public class WhileRandom {
     public static void main(String[] args) {
         int i = 0;
         while(i < 100) {
-            System.out.println((int)(Math.random() * 1000));
+            int j = 0;
+            while (j < 10) {
+                System.out.print((int)(Math.random() * 1000) + 1 + "\t");
+                j++;
+            }
+            System.out.print("\n");
             i++;
         }
     }
