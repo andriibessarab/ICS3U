11b577a8b42e29e4cd64a553a3daa5f6ac288851
andriibessarab <andriibessarab@gmail.com>
2022-11-30 12:02:12 -0500

    3.4 - WhileLoop1

---
+++ WhileLoop1.java
@@ -0,0 +1,10 @@
+public class WhileLoop1 {
+    public static void main(String[] args) {
+        // print number 1 to 300 using while
+        int i = 1;
+        while (i <= 300) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
