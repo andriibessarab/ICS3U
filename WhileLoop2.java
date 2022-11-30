f254373e0b57a9e7fd9299d5599ce26801c74b24
andriibessarab <andriibessarab@gmail.com>
2022-11-30 12:04:27 -0500

    3.4 - WhileLoop2

---
+++ WhileLoop2.java
@@ -0,0 +1,9 @@
+public class WhileLoop2 {
+    public static void main(String[] args) {
+        int i = 50;
+        while (i >= 1) {
+            System.out.print(i + "\t");
+            i--;
+        }
+    }
+}
